2e57089f1321b23a581466ea6f1952bf5f64da83
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-08-16 15:41:28 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,41 @@
 class Artist
 
-  attr_reader :name
+  attr_reader :name, :years_active
+  @@all = []
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|paint| paint.artist ==  self}
+
+  end
+
+  def galleries
+    paintings.map {|paint| paint.gallery}
+    #binding.pry
+  end
+
+  def cities
+    galleries.map {|gallery| gallery.city}
+  end
+
+  def self.average_years
+    jesus =Artist.all.map{|info| info.years_active}
+    holy = jesus.inject {|sum,n| sum+n}
+    holy /= jesus.length
+
+
+    #binding.pry
+  end
+
 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,26 @@ def reload
   load 'config/environment.rb'
 end
 
+van_gogh = Artist.new("van gogh", 20)
+da_vinci = Artist.new("da vinci",  40)
+
+moma =  Gallery.new("MOMA", "New York City")
+louvre = Gallery.new("Louvre","Paris" )
+
+paint_1 = Painting.new("starry_night", "impressionism", van_gogh, moma)
+paint_2 = Painting.new("mona_lisa", "portrait", da_vinci, louvre)
+
+
+
+
+Painting.style
+da_vinci.paintings
+da_vinci.galleries
+da_vinci.cities
+Artist.average_years
+Gallery.cities
+moma.paintings
+moma.names
+moma.yrs
 binding.pry
+ 0
