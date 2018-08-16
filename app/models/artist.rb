b5414f6ef034f21b698038da814efbd4da8aa7dc
github username <github email address>
2018-08-16 16:48:32 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,37 @@
 class Artist
-
-  attr_reader :name
+  @@all = []
+  attr_reader :name, :years_active
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
   end
+  def self.all# Get a list of all artists
+    @@all
+  end
+  def paintings # Get a list of all the paintings by a specific(keywrod for instance method) artists
+    Painting.all.select{|list| list.artist == self}
+    # binding.pry
 
+  end
+  # Get a list of all the galleries that a specific artist has paintings in
+  def gallaries
+    paintings.map{|paint| paint.gallery}
+
+  end
+# Get a list of all cities that contain galleries that a specific artist has paintings in
+  def cities
+    gallaries.map{|place| place.city}
+
+  end
+# Find the average years of experience of all artists
+  def artists
+    beef = Artist.all.map{|exp| exp.years_active} #all artist
+    beef_tot = beef.inject{|sum, n| sum + n} #sum of all
+    avg = (beef_tot / beef.length).round(0) #avg of the num of elements(beef_tot) divide by number of arrays we summed up (beef.length)
+    # binding.pry
+
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,30 @@ def reload
   load 'config/environment.rb'
 end
 
+artist1 = Artist.new("van gogh", 10)
+artist2 = Artist.new("da vinci", 8)
+
+
+
+gallery1 = Gallery.new("Moma", "NYC")
+gallery2 = Gallery.new("louvre", "Paris")
+
+# painting
+painting1 = Painting.new("starry_night", "impressionism", artist1, gallery1)
+painting2 = Painting.new("mona_lisa", "portrait", artist2, gallery2)
+# painting3 = Painting.paint_style
+# artist1 and artist2 are object that take in the new artist arguments and must be
+#replaced in painting such that it returns artists associated with the painting
+artist_paint = artist1.paintings #instance variable call variable.instance_method
+artist_gall = artist1.gallaries #variable eq artist class variable dot method in that class
+list_city = artist1.cities
+avg_age = artist1.artists
+
+gal_list = gallery1.cities
+gal_paint = gallery2.paintings # accoutn for the helper method by calling for it first in gallery calss
+gal_art_nm = gallery2.artists
+gal_avg = gallery1.gal_artist
+
 binding.pry
+
+0
