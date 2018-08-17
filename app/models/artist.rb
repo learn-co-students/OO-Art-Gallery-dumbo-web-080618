ace42349ef52eabc694d73b2aab71cc729c78304
vavnilov <vadim.avnilov@gmail.com>
2018-08-17 16:37:34 -0400

    Completed

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,35 @@
 class Artist
 
-  attr_reader :name
+  @@all = []
+
+  attr_reader :name, :years_active
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    self.paintings.map {|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    self.galleries.map {|gallery| gallery.city}.uniq
+  end
+
+  def self.average_experience
+    total_experience = 0
+    all.each {|artist| total_experience += artist.years_active}
+    total_experience.to_f/(all.count)
   end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,16 @@ def reload
   load 'config/environment.rb'
 end
 
+vadim = Artist.new("Vadim", 7)
+joy = Artist.new("Joy Huang", 3)
+otabek = Artist.new("Otash",12)
+
+leouve = Gallery.new("The leove", "Paris")
+moma = Gallery.new("Museum of Modern Art", "NYC")
+metropolitan = Gallery.new("Metropolitan Museum of Art","NYC")
+
+my_art = Painting.new("Vadim's art", "contemporary", vadim, leouve)
+joys_art = Painting.new("Joy's Masterpiece", "modern", joy, moma)
+joys_other_art = Painting.new("Whoa another one", "ancient", joy, metropolitan)
+russian = Painting.new("Uzbeki Winters", "abstract", otabek,metropolitan)
 binding.pry
