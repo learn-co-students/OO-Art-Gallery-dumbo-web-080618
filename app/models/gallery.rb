87e2ea4a1d43f81312558286b267b8c0da8d1c31
Linh <linh44h@gmail.com>
2018-08-16 22:47:31 -0400

    done

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,10 +2,37 @@ class Gallery
 
   attr_reader :name, :city
 
-def initialize(name, city)
-  @name = name
-  @city = city
-end
+  @@all = []
 
+  def initialize(name, city)
+    @name = name
+    @city = city
+    @@all << self
+  end
+
+  #Get a list of all galleries
+  def self.all
+    @@all
+  end
+
+  #Get a list of all cities that have a gallery. A city should not appear more than once in the list.
+  def self.list_cities
+    self.all.map { |c| c.city }.uniq
+  end
+
+  #Get a list of artists that have paintings at a specific gallery
+  def my_painting
+    Painting.all.select {|p| p.gallery == self}
+  end
+
+  #Get a list of the names of artists that have paintings at a specific gallery
+  def list_artists
+    self.my_painting.map { |a| a.artist.name }.uniq
+  end
+
+  #Get the combined years of experience of all artists at a specific gallery
+  def artist_years
+    self.my_painting.map { |a| a.artist.years_active }.reduce(:+)
+  end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,26 @@
 class Painting
 
   attr_reader :title, :style
+  attr_accessor :artist, :gallery
 
-  def initialize(title, style)
+  @@all = []
+
+  def initialize(title, style, artist, gallery)
     @title = title
     @style = style
+    @artist = artist
+    @gallery = gallery
+    @@all << self
+  end
+
+  #Get a list of all paintings
+  def self.all
+    @@all
+  end
+
+  #Get a list of all painting styles (a style should not appear more than once in the list)
+  def self.painting_style
+    self.all.map { |p| p.style }.uniq
   end
 
 end
