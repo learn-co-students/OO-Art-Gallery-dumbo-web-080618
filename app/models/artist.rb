61cc0c8d8a036f5a3a561ab4036be585e8724931
skarimo <karimovj@gmail.com>
2018-08-18 14:26:50 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,41 @@
+
 class Artist
 
-  attr_reader :name
+  attr_reader :name, :years_active
+  @@all = []
+
+  def self.all
+    @@all
+  end
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
   end
+
+  def paintings
+    Painting.all.select{|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map{|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    galleries.map{|gallery| gallery.city}
+  end
+
+  def self.average_years_active
+    ages = @@all.map{|artist| artist.years_active}
+    (ages.reduce(:+)).to_f/ages.length
+  end
+
+
+
+
+
+
 
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,23 @@
+require 'pry'
 class Painting
 
-  attr_reader :title, :style
+  attr_reader :title, :style, :artist, :gallery
+  @@all = []
+  def self.all
+    @@all
+  end
 
-  def initialize(title, style)
+
+  def initialize(title, style, artist, gallery)
     @title = title
     @style = style
+    @artist = artist
+    @gallery = gallery
+    @@all << self
+  end
+
+  def self.painting_styles
+    (self.all.map{|painting| painting.style}).uniq
   end
 
 end
