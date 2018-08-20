772b44ef3a893b6c8f5da7d93f02d6e12945dbac
Madeline Celis <celiscb@vcu.edu>
2018-08-19 21:10:15 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,19 @@ def reload
   load 'config/environment.rb'
 end
 
+maddieArtist = Artist.new('MaddieArtist', 5)
+jeffreyArtist = Artist.new('JeffreyArtist', 6)
+georgeArtist = Artist.new('GeorgeArtist', 2)
+
+galleryOne = Gallery.new('GalleryOne', 'New York')
+galleryTwo = Gallery.new('galleryTwo', 'DC')
+
+
+brightPainting = Painting.new('Bright Painting', 'abstract', maddieArtist, galleryOne)
+sunPainting = Painting.new('Sun Painting', 'impressionist', jeffreyArtist, galleryOne)
+bluePainting = Painting.new('Blue Painting', 'finger painting', georgeArtist, galleryTwo)
+
+
+
+
 binding.pry
