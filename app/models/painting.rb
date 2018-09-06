d9491e177293867ea616e1d59ec807429945b0ca
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-09-06 18:20:46 -0400

    reset

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,24 +1,4 @@
 class Painting
 
-  attr_reader :title, :style, :artist, :gallery
-
-  @@all = []
-
-  def initialize(title,style,artist,gallery)
-    @title = title
-    @style = style
-    @artist = artist
-    @gallery = gallery
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.style
-    paint = Painting.all.map { |paints| paints.style}
-    paint.uniq
-  end
 
 end
